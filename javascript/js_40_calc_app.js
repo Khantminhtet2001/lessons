7c71b39f30b2d0ae61e4325fc9ae64cf6cbe1178
Khant Min Htet <khantminhtet2001onek@gmail.com>
2024-03-17 15:39:50 +0630

    add calc part 4

--- javascript/js_40_calc_app.js
+++ javascript/js_40_calc_app.js
@@ -1,9 +1,47 @@
 let keypad = document.querySelector(".key");
 let displayBox = document.querySelector(".display");
+let operators = ["+","-","*","/","%"];
 
-let showDisplay = (x) => displayBox.innerText += x
+let filter = x =>{
+    let current = displayBox.innerText;
+    let lastChar = current[current.length-1];
+    if (current == '0'){
+        if(x != "."){
+            clearLast();
+        }
 
-let calc = () => displayBox.innerText = eval(displayBox.innerText);
+        
+    }
+    if(operators.includes(x)){
+        if(operators.includes(lastChar)){
+            return false;
+        }
+    }
+    return true;
+}
+
+// let calcFiler = x =>{
+//     let current = displayBox.innerText;
+//     let lastChar = current[current.length-1];
+//     if(operators.includes(lastChar) == operators.includes()){
+//         return false;
+//     }
+//     return true;
+// }
+
+
+
+let showDisplay = (x) => {
+    if(filter(x)) {
+        displayBox.innerText += x;
+    }
+}
+
+let calc = () => {
+    // if(calcFiler(x)){
+        displayBox.innerText = eval(displayBox.innerText);
+    // }
+}
 
 let clearAll = () => displayBox.innerText = '';
 
