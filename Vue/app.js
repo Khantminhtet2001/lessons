210a36c8d6160dc5b3ca59094683ad7220fdc067
Khant Min Htet <khantminhtet2001onek@gmail.com>
2024-04-01 00:35:14 +0630

    add vue part 1

--- Vue/app.js
+++ Vue/app.js
@@ -1,3 +1,33 @@
 // alert("Error");
 
 console.log("Khant Min Htet");
+
+//Variables
+
+let name = "Khant Min Htet";
+var age = 42;
+const PI = 3.14159;
+
+
+//DataType
+
+//string
+let name2 = "Khant Min Htet";
+
+//stirng + number = string;
+
+//number
+let age = 42;
+
+//boolean
+let isMale = true;
+
+//null
+let emptyCup = null;
+console.log(emptyCup);
+
+//undefined
+let unKnownVar ;
+
+
+
